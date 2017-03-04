c01f736f799d0c6bd9ef4c775920bec19ec17418
FranciniSilva <franciniassis89@hotmail.com>
2017-03-04 12:14:44 -0300

    Criando component principal da aplicação

---
+++ client/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+//Notação do tipo decorator
+@Component({
+    selector: 'app',
+    templateUrl: './app/app.component.html'
+})
+export class AppComponent { }
